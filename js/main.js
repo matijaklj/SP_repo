f409598fbafdd5a7f94ba73e7d0889ac8a097bb1
matijaklj <matija.kljun@gmail.com>
2016-11-27 11:34:52 +0100

    fixing responsive desing, javascript and stuff

--- js/main.js
+++ js/main.js
@@ -98,85 +98,111 @@ function submitPost(event) {
 */
 }
 
+function readPosts(event) {
+    var filename = "json/posts"; //name od the .json file with posts
+
+    var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", postHandlerJson);
+	oReq.open("GET", filename+".json");
+	oReq.responseType = "text";
+	oReq.send();
+
+	event.preventDefault();
+}
+
 function postHandlerJson(event) {
     var postJson = JSON.parse(this.responseText);
-	coverImage = postJson["cover-image"];
-	profileImage = postJson["profile-image"];
-    username = postJson["username"];
-    text = postJson["text"];
-    loc = postJson["location"];
 
-    if(loc == "") console.log(loc);
+    for (var i = 0; i < postJson.length; i++) {
+        coverImage = postJson[i]["cover-image"];
+    	profileImage = postJson[i]["profile-image"];
+        username = postJson[i]["username"];
+        text = postJson[i]["text"];
+        loc = postJson[i]["location"];
 
-
-    createArticlePost(coverImage, profileImage, username, text, loc, function(post) {
-        document.getElementById("newsfeed").insertBefore(post, document.getElementById("newsfeed").firstChild);
-    });
-
+        createArticlePost(coverImage, profileImage, username, text, loc, function(post) {
+            document.getElementById("newsfeed").insertBefore(post, document.getElementById("newsfeed").firstChild);
+        });
+    }
 }
 
 function createArticlePost(coverImage, profileImage, username, text, loc, callback) {
-    var art = document.createElement("article");
-    art.className += "post";
-    var div1 = document.createElement("div");
-    div1.className += "cover-image";
-    var cover_img = document.createElement("img");
-    cover_img.className += "cover-image";
-    cover_img.setAttribute("src", coverImage);
-    cover_img.setAttribute("alt", "cover photo");
-    div1.appendChild(cover_img);
-    art.appendChild(div1);
+    checkText(text, function(text) {
 
-    var div2 = document.createElement("div");
-    div2.className += "profile-image-div";
-    var profile_img = document.createElement("img");
-    profile_img.className += "profile-image";
-    profile_img.setAttribute("src", profileImage);
-    profile_img.setAttribute("alt", "profile photo");
-    div2.appendChild(profile_img);
-    art.appendChild(div2);
+        var art = document.createElement("article");
+        art.className += "post";
+        var div1 = document.createElement("div");
+        div1.className += "cover-image";
+        var cover_img = document.createElement("img");
+        cover_img.className += "cover-image";
+        cover_img.setAttribute("src", coverImage);
+        cover_img.setAttribute("alt", "cover photo");
+        div1.appendChild(cover_img);
+        art.appendChild(div1);
 
-    var h2 = document.createElement("h2");
-    h2.className += "profile-name";
-    h2.innerHTML = username;
-    art.appendChild(h2);
+        var div2 = document.createElement("div");
+        div2.className += "profile-image-div";
+        var profile_img = document.createElement("img");
+        profile_img.className += "profile-image";
+        profile_img.setAttribute("src", profileImage);
+        profile_img.setAttribute("alt", "profile photo");
+        div2.appendChild(profile_img);
+        art.appendChild(div2);
 
-    var div3 = document.createElement("div");
-    div3.className += "post-text";
-    var p = document.createElement("p");
-    p.innerHTML = text;
-    div3.appendChild(p);
-    art.appendChild(div3);
+        var h2 = document.createElement("h2");
+        h2.className += "profile-name";
+        h2.innerHTML = username;
+        art.appendChild(h2);
 
-    if(loc != "") {
-        var tags = document.createElement("div");
-        tags.className += "tags";
-        var a = document.createElement("a");
-        a.setAttribute("href", loc);
-        var span = document.createElement("span");
-        span.className += "buttn";
-        var icon = document.createElement("div");
-        icon.className += "sprite sprite-location";
-        span.appendChild(icon);
-        span.innerHTML += " Location";
-        a.appendChild(span);
-        tags.appendChild(a);
+        var div3 = document.createElement("div");
+        div3.className += "post-text";
+        var p = document.createElement("p");
+        p.innerHTML = text;
+        div3.appendChild(p);
+        art.appendChild(div3);
 
-        art.appendChild(tags);
-    }
+        if(loc != "") {
+            var tags = document.createElement("div");
+            tags.className += "tags";
+            var a = document.createElement("a");
+            a.setAttribute("href", loc);
+            var span = document.createElement("span");
+            span.className += "buttn";
+            var icon = document.createElement("div");
+            icon.className += "sprite sprite-location";
+            span.appendChild(icon);
+            span.innerHTML += " Location";
+            a.appendChild(span);
+            tags.appendChild(a);
 
-    if(callback != null) callback(art);
+            art.appendChild(tags);
+        }
+
+        if(callback != null) callback(art);
+    });
+}
+
+function checkText(text, callback) {
+
+    text = text.replace(/(^|\s)(#[a-z\d-]+)/gi, "$1<a href='#'>$2</a>");
+    text = text.replace(/(^|\s)(@([a-z\d-]+))/gi, "$1<a href=\"#$3\">$2</a>");
+
+    if(callback != null) callback(text);
 }
 
 function collapseMenu(event) {
-    document.getElementById("show-menu").className = "header-link-colapsed buttn";
-    document.getElementById("collapse-menu").className = "header-link-colapsed buttn no-display";
-    //document.getElementById("show-menu").style.display = "block";
-    //document.getElementById("collapse-menu").style.display = "none";
+    //console.log(document.getElementById("collapse-menu").getAttribute("href") );
+    if(document.getElementById("collapse-menu").getAttribute("href")  == "#dropdown-menu")
+        document.getElementById("collapse-menu").setAttribute("href", "#")
+    else
+        document.getElementById("collapse-menu").setAttribute("href", "#dropdown-menu");;
+
+    //document.getElementById("show-menu").className = "header-link-colapsed buttn";
+    //document.getElementById("collapse-menu").className = "header-link-colapsed buttn no-display";
+
 }
 function showMenu(event) {
     document.getElementById("collapse-menu").className = "header-link-colapsed buttn";
     document.getElementById("show-menu").className = "header-link-colapsed buttn no-display";
-    //document.getElementById("show-menu").style.display = "none";
-    //document.getElementById("collapse-menu").style.display = "block";
+
 }
